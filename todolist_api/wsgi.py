6d5aac5d3dc5e20d2a9b1c568f0d2d2c495ff3e2
Russell <russell.tchamba@gmail.com>
2023-01-29 21:10:53 +0100

    wsg failure

--- todolist_api/wsgi.py
+++ todolist_api/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+""" import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolist_api.settings')
 
-application = get_wsgi_application()
+application = get_wsgi_application() """
+
+
+from os import path
+import sys
+
+
+if path not in sys.path:
+    sys.path.append(path)
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
